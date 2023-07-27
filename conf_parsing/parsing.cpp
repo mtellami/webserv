c1bbdd33d9bb2d19885ed2b25d77b081d7909d44
laila <chokri.laila.ch@gmail.com>
2023-07-27 11:23:43 +0100

    Creating a repo for the team

---
+++ conf_parsing/parsing.cpp
@@ -0,0 +1,14 @@
+#include "parsing.hpp"
+
+servers::servers()
+{
+}
+
+servers::~servers()
+{
+}
+
+locations::locations() {}
+
+locations::~locations() {}
+
---
+++ conf_parsing/parsing.hpp
@@ -0,0 +1,46 @@
+#ifndef PARSING_HPP
+#define PARSING_HPP
+
+#include <algorithm>
+#include <cctype>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+
+class locations {
+  private:
+  public:
+    std::string pattern;
+    std::string methods;
+    std::string redir_path;
+    std::string root;
+    bool autoindex;
+    std::vector<std::string> def;
+    std::vector<std::pair<std::string, std::string> > cgi; // vec.push_back(make_pair(string, string2);
+    locations();
+    ~locations();
+};
+
+class servers {
+  private:
+  public:
+    std::vector<locations> loc;
+    std::string address;
+    std::string host;
+    std::string server_name;
+    // int client_max_body_size;
+    std::string client_max_body_size;
+    std::map<int, std::string> error_pages;
+
+    // error pages are missing here;
+    servers();
+    ~servers();
+};
+//should be done:
+//    store err pages 
+//    check for invalid args in other attributes/amount
+
+#endif
