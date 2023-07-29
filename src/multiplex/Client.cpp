8553d5e98a614fd900c894b688d07ee9ea91053d
Moad Tellami <mtellami@e1r6p1.1337.ma>
2023-07-29 19:19:59 +0100

    read request header, read body(..)

--- src/multiplex/Client.cpp
+++ src/multiplex/Client.cpp
@@ -6,7 +6,7 @@
 /*   By: mtellami <mtellami@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/23 08:42:38 by mtellami          #+#    #+#             */
-/*   Updated: 2023/07/29 13:49:16 by mtellami         ###   ########.fr       */
+/*   Updated: 2023/07/29 19:18:21 by mtellami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ Client::Client(Cluster *cluster) : _cluster(cluster) {
     _socket = accept(_cluster->get_listen_fd(), (struct sockaddr *)_cluster->get_address(), (socklen_t*)_cluster->get_addrlen());
     _req = new Request;
     _req->_fd = -1;
+    _req->_i = 0;
     _req->_recv_header = false;
     _req->_downloading = false;
     _req->_body_size = 0;
@@ -35,11 +36,10 @@ SOCK_FD Client::get_connect_fd() {
 }
 
 void    Client::parse_header() {
-    size_t      trim = _recv_buffer.find("\r\n\r\n");
-    std::string header(_recv_buffer.substr(0, trim));
+    std::string header(_recv_buffer.substr(0, _recv_buffer.find("\r\n\r\n")));
+    // std::cout << header << std::endl;
+    // exit(0);
 
-    _recv_buffer = _recv_buffer.substr(trim + 4);
-    // handle POST request
     std::istringstream iss(header);
     std::string line;
 
@@ -59,37 +59,57 @@ void    Client::parse_header() {
 }
 
 void    Client::recv_body(void) {
-    if (!_req->_downloading) {
-        _req->_downloading = true;
-        _req->_fd = open("upload.png", O_CREAT | O_TRUNC);
-    }
-    if (_req->_method != "GET") {
-        if (stoi(_req->_header.find("Content-Length")->second) >= _req->_i) {
-            write(_req->_fd, _recv_buffer.c_str(), strlen(_recv_buffer.c_str()));
-            close(_req->_fd);
-        }
-    }
-    _done_recv = true;
-    std::cout << "Still reading ......" << std::endl;
+    if (_done_recv)
+        return;
+    _req->_fd = open("upload.jpg", O_CREAT | O_RDWR, 0666);
+    write(_req->_fd, _recv_buffer.c_str(), strlen(_recv_buffer.c_str()));
+    close(_req->_fd);
 }
 
 // call this function when the client is readyToWrite in 
 void    Client::recieve(void) {
-    _req->_i = recv(_socket, _req->_buffer, SIZE, 0);
-    if (_req->_i == -1)
-        throw System();
-    if (!_req->_i || _done_recv) {
-        _done_recv = true;
-        return;
+    if (_done_recv)
+        return ;
+    if (!_req->_recv_header) {
+        while (_recv_buffer.find("\r\n\r\n") == std::string::npos) {
+            _req->_i = recv(_socket, _req->_buffer, 1, 0);
+            if (_req->_i == -1)
+                throw System();
+            if (!_req->_i) {
+                _done_recv = true;
+                return;
+            }
+            _recv_buffer += std::string(_req->_buffer, _req->_i);
+        }
+        parse_header();
+        _req->_recv_header = true;
+    }else {
+        int i = 0;
+        while (i < stoi(_req->_header.find("Content-Length")->second) && i < SIZE) {
+            _req->_i = recv(_socket, _req->_buffer, 1, 0);
+            if (_req->_i == -1)
+                throw System();
+            if (!_req->_i) {
+                _done_recv = true;
+                return;
+            }
+            _recv_buffer += std::string(_req->_buffer, _req->_i);
+            i++;
+        }
+        std::cout << "Uploading .............." << std::endl;
+        if (i == stoi(_req->_header.find("Content-Length")->second)) { // never be here ..
+            _done_recv = true;
+            recv_body();
+        }
     }
+
     _recv_buffer += std::string(_req->_buffer, _req->_i);
-    if (!_req->_recv_header) { // still not recieve the header
+    if (!_req->_recv_header) {
         if (_recv_buffer.find("\r\n\r\n") != std::string::npos) {
             _req->_recv_header = true;
             _req->_i = _recv_buffer.substr(_recv_buffer.find("\r\n\r\n")).length();
-            parse_header();
         }
-    } else { // recieve the body in POST request)
+    } else {
         recv_body();
     }
 }
